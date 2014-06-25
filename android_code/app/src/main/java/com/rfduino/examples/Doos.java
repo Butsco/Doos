40a144e3984fab5eb890f5c19aee30ca7ff35b5d
Bert Wijnants <bert.wijnants@gmail.com>
2014-06-25 02:21:08 +0200

    right version now

---
+++ android_code/app/src/main/java/com/rfduino/examples/Doos.java
@@ -0,0 +1,173 @@
+package com.rfduino.examples;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.rfduino.R;
+import com.rfduino.core.BluetoothLEStack;
+import com.rfduino.core.RFDuinoSystemCharacteristics;
+
+import java.util.List;
+import java.util.Map;
+
+/** 
+ * LedButtonExample.java
+ * 
+ * This example Activity is designed to be run with a RFDuino board that has had the "RFDuinoBLE>LedButton" sketch loaded onto
+ * it.  See https://github.com/abolger/awesomesauce-rfduino/wiki/Getting%20Started for more details. 
+ * 
+ * @author adrienne
+ * 
+ * This library is released under the LGPL. A copy of the license should have been distributed with this library/source code,
+ *  if not, you can read it here: (https://github.com/abolger/awesomesauce-rfduino/blob/master/LICENSE)
+*/
+public class Doos extends Activity {
+	BluetoothLEStack rfduinoConnection;
+	BluetoothDevice chosenBluetoothDevice;
+	TextView buttonPressEventDisplay;
+    MediaPlayer mMediaPlayer;
+	
+	/** Tell the Bluetooth manager what to do if user decides not to connect anymore. **/
+	OnCancelListener onCancelConnectionAttempt = new OnCancelListener(){
+		@Override
+		public void onCancel(DialogInterface arg0) {
+			Doos.this.finish();
+		}
+	};
+
+    /**
+	 * Creates our main layout for this page (A checkbox that should stay synced with the RFDuino)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.play_sound);
+
+        mMediaPlayer = MediaPlayer.create(this, R.raw.badabing);
+
+		buttonPressEventDisplay = (TextView) findViewById(R.id.buttonPressNotification);
+
+		//Get the bluetooth device that we put here: this comes from right before we started this activity on the "ListAllExamples.java" screen. 
+		chosenBluetoothDevice = (BluetoothDevice) getIntent().getExtras().get("bluetooth_device");
+		Log.i(BluetoothLEStack.logTag, "Chosen device is"+ chosenBluetoothDevice);
+		
+		rfduinoConnection = BluetoothLEStack.connectToBluetoothLEStack(chosenBluetoothDevice, 
+				this,
+				RFDuinoSystemCharacteristics.RFDUINO_PROFILE_SERVICE_UUID,
+				onCancelConnectionAttempt
+				);
+		
+		backgroundTask.postDelayed(runOnceConnected, 1000l);
+		
+	}
+	
+	private Runnable runOnceConnected = new Runnable(){
+		public void run(){
+		if (rfduinoConnection.isConnected()){
+			List<String> availableUUIDs = rfduinoConnection.getDiscoveredCharacteristics();
+			if (availableUUIDs != null && availableUUIDs.contains(RFDuinoSystemCharacteristics.RFDUINO_PROFILE_RECEIVE_UUID) ){
+				//Connected and have the service handle. Register a callback to use the service:
+				rfduinoConnection.setOnCharacteristicChangedWatcher(RFDuinoSystemCharacteristics.RFDUINO_PROFILE_RECEIVE_UUID, buttonPressedCallback);
+			} else {
+				rfduinoConnection.discoverAvailableCharacteristics();
+				backgroundTask.postDelayed(this, 1000l);
+			}
+		}else {
+			backgroundTask.postDelayed(this, 1000l);
+		}
+		}
+	};
+	
+	
+	Handler backgroundTask = new Handler();
+	
+	//A slightly ridiculous nested callback that can be passed to our Bluetooth manager:
+	private Runnable buttonPressedCallback = new Runnable(){
+		public void run(){
+			runOnUiThread(new Runnable(){
+				public void run(){
+					Log.i("LedButtonExample", "Button was pressed.");
+
+                    Map <String, byte[]> response = rfduinoConnection.getLatestCharacteristics();
+                    Log.e("response", ""+response);
+
+					String newText = buttonPressEventDisplay.getText() + "*";
+					buttonPressEventDisplay.setText(newText);
+
+                    mMediaPlayer.start();
+				}
+				
+			});
+			
+		}
+		
+	};
+	
+	
+
+	
+	
+	private Runnable turnOff =  new Runnable(){
+		public void run(){
+			
+		//Once we're sure the device offers the ability to write to it, send out the command to write ZERO to the RFDuino:
+		List<String> availableUUIDs = rfduinoConnection.getDiscoveredCharacteristics();
+		if (availableUUIDs != null && availableUUIDs.contains(RFDuinoSystemCharacteristics.RFDUINO_PROFILE_SEND_UUID)){
+			//Try to write the chosen color into this characteristic to send back:
+			byte[] sendZero = new byte[]{0};
+			rfduinoConnection.writeCharacteristic(RFDuinoSystemCharacteristics.RFDUINO_PROFILE_SEND_UUID, sendZero);
+		}
+		}
+	};
+	
+	private Runnable turnOn =  new Runnable(){
+		public void run(){
+			
+		//Once we're sure the device offers the ability to write to it, send out the command to write ONE to the RFDuino:
+		List<String> availableUUIDs = rfduinoConnection.getDiscoveredCharacteristics();
+		if (availableUUIDs != null && availableUUIDs.contains(RFDuinoSystemCharacteristics.RFDUINO_PROFILE_SEND_UUID)){
+			//Try to write the chosen color into this characteristic to send back:
+			byte[] sendOne = new byte[]{1};
+			rfduinoConnection.writeCharacteristic(RFDuinoSystemCharacteristics.RFDUINO_PROFILE_SEND_UUID, sendOne);
+		}
+		}
+	};
+
+	@Override 
+	public void onDestroy(){
+		backgroundTask.removeCallbacks(turnOn);
+		backgroundTask.removeCallbacks(turnOff);
+		backgroundTask.removeCallbacks(runOnceConnected);
+		backgroundTask.removeCallbacks(buttonPressedCallback);
+		
+		rfduinoConnection.disconnect();
+		super.onDestroy();
+	}
+
+	@Override 
+	public void onResume(){
+		if (rfduinoConnection == null){
+			rfduinoConnection = BluetoothLEStack.connectToBluetoothLEStack(chosenBluetoothDevice,
+					this,
+					RFDuinoSystemCharacteristics.RFDUINO_PROFILE_RECEIVE_UUID,
+					onCancelConnectionAttempt
+				);
+		}
+		super.onResume();
+	}
+
+    @Override
+    public void onBackPressed() {
+        // kill the activity when back is pressed.
+        super.onBackPressed();
+        this.finish();
+    }
+	
+}
---
+++ android_code/app/src/main/java/com/rfduino/examples/MainActivity.java
@@ -0,0 +1,191 @@
+package com.rfduino.examples;
+
+import com.rfduino.R;
+import com.rfduino.core.BluetoothLEStack;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.view.View;
+import android.app.ListActivity;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * ListAllExamples.java
+ * 
+ * This Activity:
+ * 1.  loads a list of all possible example Activities for use with an RFDuino board and displays them in a clickable format
+ * 2. Allows the user to select an Activity via a listElementListener.
+ * 3. Performs a Bluetooth Low Energy device scan and displays available Bluetooth devices that can be used with the Activity
+ * 4. Stores the selected BluetoothDevice as an "Extra" to pass to the new Activity's "onCreate" method when it is initialized.
+ *   
+ * @author adrienne
+ * 
+ * This library is released under the LGPL. A copy of the license should have been distributed with this library/source code,
+ *  if not, you can read it here: (https://github.com/abolger/awesomesauce-rfduino/blob/master/LICENSE)
+*/
+
+public class MainActivity extends Activity {
+
+	Intent chosenExample;
+	BluetoothDevice chosenBluetoothDevice;
+    TextView mBluetoothStatusText;
+    Boolean turnOnBluetoothWhenOff = false;
+    Boolean searchWhenBluetoothTurnedOn = true;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+        mBluetoothStatusText = (TextView) findViewById(R.id.bluetoothStatusText);
+        Button connectButton = (Button) findViewById(R.id.connectButton);
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restartBluetoothAndSearch();
+            }
+        });
+
+        if (isBluetoothOn()) {
+            setBluetoothStatusText("Bluetooth on");
+        } else {
+            setBluetoothStatusText("Bluetooth off");
+        }
+
+        // Register for broadcasts on BluetoothAdapter state change
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        this.registerReceiver(mReceiver, filter);
+	}
+
+    private void restartBluetoothAndSearch() {
+
+        if (isBluetoothOn()) {
+            turnOnBluetoothWhenOff = true;
+            setBluetooth(false);
+        } else {
+            turnOnBluetoothAndSearch();
+        }
+    }
+
+    private void turnOnBluetoothAndSearch() {
+        if (!isBluetoothOn()) {
+            searchWhenBluetoothTurnedOn = true;
+            setBluetooth(true);
+        } else {
+            searchForDevices();
+        }
+    }
+
+    private void searchForDevices() {
+        BluetoothLEStack.beginSearchingForBluetoothDevices(MainActivity.this);
+        chosenExample = new Intent(MainActivity.this, Doos.class);
+
+        //Runs code that pops up a second list on the UI screen- this one shows all possible bluetooth devices that we can use in our examples.
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run() {
+                BluetoothLEStack.showFoundBluetoothDevices(MainActivity.this, rfduinoChosenListener);
+            }
+        });
+    }
+
+    private boolean setBluetooth(boolean enable) {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        boolean isEnabled = bluetoothAdapter.isEnabled();
+        if (enable && !isEnabled) {
+            return bluetoothAdapter.enable();
+        }
+        else if(!enable && isEnabled) {
+            return bluetoothAdapter.disable();
+        }
+        // No need to change bluetooth state
+        return true;
+    }
+
+    private boolean isBluetoothOn() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return bluetoothAdapter.isEnabled();
+    }
+
+    private void setBluetoothStatusText(final String text) {
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run() {
+                mBluetoothStatusText.setText(text);
+            }
+        });
+    }
+	
+	@Override 
+	public void onDestroy(){
+		BluetoothLEStack.stopSearchingForBluetoothDevices(this);
+        this.unregisterReceiver(mReceiver);
+		super.onDestroy();
+	}
+	
+	
+	/** 
+	 * GUI OnClickListener: after a list of possible RFDuinos is displayed, this handler listens for a click in the list and connects
+	 * to the corresponding radio. 
+	 */
+	DialogInterface.OnClickListener rfduinoChosenListener = new DialogInterface.OnClickListener() {
+		
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			BluetoothLEStack.stopSearchingForBluetoothDevices(MainActivity.this);
+			
+			chosenBluetoothDevice = BluetoothLEStack.discoveredDevices.get(which);
+			chosenExample.putExtra("bluetooth_device", chosenBluetoothDevice);
+			
+			startActivity(chosenExample);
+		}
+	};
+
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        setBluetoothStatusText("Bluetooth off");
+                        if (turnOnBluetoothWhenOff) {
+                            turnOnBluetoothAndSearch();
+                            turnOnBluetoothWhenOff = false;
+                        }
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        setBluetoothStatusText("Turning Bluetooth off...");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        setBluetoothStatusText("Bluetooth on");
+                        if (searchWhenBluetoothTurnedOn) {
+                            searchForDevices();
+                            searchWhenBluetoothTurnedOn = false;
+                        }
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        setBluetoothStatusText("Turning Bluetooth on...");
+                        break;
+                }
+            }
+        }
+    };
+}
